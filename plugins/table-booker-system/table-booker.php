06293282031c2a38a40abdc0538413f4b328f4f0
Nate.Waldschmidt@bps101.net <nathaniel.waldsch@gmail.com>
2022-01-16 21:25:25 -0600

    Removed this method because it is not going to be used.

--- plugins/table-booker-system/table-booker.php
+++ plugins/table-booker-system/table-booker.php
@@ -138,38 +138,6 @@ if (!class_exists('TB_Init')) {
         }
 
         /**
-         * Creates the reservation post type 
-         * which would be a child of the 
-         * restaurant post type.
-         * 
-         * @static
-         */
-        // static function add_reservation_post_type():void {
-        //     register_post_type('reservation', [
-        //         'labels' => [
-        //             'name'          => 'Reservations',
-        //             'singular_name' => 'Reservation',
-        //             'add_new'       => 'Add New Reservation',
-        //             'add_new_item'  => 'Add New Reservation',
-        //             'edit_item'     => 'Edit Reservation',
-        //             'all_items'     => 'All Reservations',
-        //             'search_items'  => 'Search Reservations',
-        //         ],
-        //         'public'              => false,
-        //         'hierarchical'        => true,
-        //         'publicly_queryable'  => false,
-        //         'exclude_from_search' => true,
-        //         'show_ui'             => true,
-        //         'show_in_menu'        => true,
-        //         'has_archive'         => true,
-        //         'rewrite'             => true,
-        //         'query_var'           => true,
-        //         'supports'            => ['title'],
-        //         'delete_with_user'    => true
-        //     ]);
-        // }
-
-        /**
          * Creates the restaurant meta box section for
          * the restaurant post type.
          * 
