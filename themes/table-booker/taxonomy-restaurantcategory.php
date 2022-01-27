4eb7ae84814fd48aa1dd909bef3bcaa2c37f8263
Nate.Waldschmidt@bps101.net <nathaniel.waldsch@gmail.com>
2022-01-27 17:21:33 -0600

    Added a basic implementation of the restaurant category view. This is responsive but is lacking a lot of details that I plan to add a later point in time.

--- themes/table-booker/taxonomy-restaurantcategory.php
+++ themes/table-booker/taxonomy-restaurantcategory.php
@@ -7,20 +7,30 @@ get_header();
 
 <?php $term = get_queried_object();  ?>
 
-<h1><?php echo esc_html($term->name); ?></h1>
+<main>
+    <h1><?php echo esc_html($term->name); ?></h1>
 
-<?php // Looks for restaurants with this category. ?>
-<?php if (have_posts()): ?>
-    <ul>
-        <?php while(have_posts()): the_post(); ?>
-            <li>
-                <a href="<?php echo esc_url(get_permalink()); ?>">
-                    <?php echo esc_html(get_the_title()); ?>
-                </a></li>
-        <?php endwhile; ?>
-    </ul>
-<?php else: ?>
-    <p>Unfortunately, there are no <?php echo esc_html($term->name); ?> restaurants.</p>
-<?php endif; ?>
+    <?php // Looks for restaurants with this category. ?>
+    <?php if (have_posts()): ?>
+        <ul class="tb-category-restaurants">
+            <?php while(have_posts()): the_post(); ?>
+                <li>
+                    <article class="tb-restaurant">
+                        <a class="tb-restaurant-title" href="<?php echo esc_url(get_permalink()); ?>">
+                            <?php echo esc_html(get_the_title()); ?>
+                        </a>
+                        <p class="tb-restaurant-description">
+                            <?php echo esc_html(get_post_meta(get_the_ID(), 'restaurant_description', true)); ?>
+                        </p>
+                    </article>
+                </li>
+            <?php endwhile; ?>
+        </ul>
+    <?php else: ?>
+        <p>Unfortunately, there are no <?php echo esc_html($term->name); ?> restaurants.</p>
+    <?php endif; ?>
+</main>
+
+
 
 <?php get_footer(); ?>
