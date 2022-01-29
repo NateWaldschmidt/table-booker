e7c8abd51d9192f1be560fc4c5fa7e0e8e175547
NateWaldschmidt <nate.waldschmidt@bps101.net>
2022-01-29 10:05:06 -0600

    Added the search functionality with the option to filter the results.

--- themes/table-booker/searchform.php
+++ themes/table-booker/searchform.php
@@ -1,19 +1,33 @@
-<form action="/" method="get" role="search">
+<form id="form-site-search" action="/" method="get" role="search">
     <label for="site-search">Search</label>
     <input
     type        ="search"
-    name        ="s"
     id          ="site-search"
+    name        ="s"
     value       ="<?php the_search_query(); ?>"
     aria-label  ="website"
     autocomplete="on"
     />
-    
+
     <button
     type      ="submit"
+    class     ="btn"
     aria-label="Submit Search"
     title     ="Submit Search"
     >
         Submit Search
     </button>
+
+    <fieldset>
+        <legend>Filter</legend>
+        <label for="ss-filt-restaurants">
+            <input type="checkbox" name="filter-restaurants" id="ss-filt-restaurants" value="restaurant">
+            Restaurants
+        </label>
+
+        <label for="ss-filt-pages">
+            <input type="checkbox" name="filter-pages" id="ss-filt-pages" value="page">
+            Pages
+        </label>
+    </fieldset>
 </form>
