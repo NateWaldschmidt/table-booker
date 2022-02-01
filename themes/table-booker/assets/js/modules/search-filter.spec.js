2f28fb88f89d410ec64d4b1843f585735721447a
NateWaldschmidt <nate.waldschmidt@bps101.net>
2022-02-01 10:24:12 -0600

    Added more tests to the search results filtering.

--- themes/table-booker/assets/js/modules/search-filter.spec.js
+++ themes/table-booker/assets/js/modules/search-filter.spec.js
@@ -1,7 +1,7 @@
 'use strict';
 import SearchFilter from './search-filter';
 
-test('The that the selector for the checkboxes is not an empty NodeList.', () => {
+test('Tests that the selector for the checkboxes is not an empty NodeList.', () => {
     document.body.innerHTML = `
         <form id="form-site-search">
             <fieldset>
@@ -22,7 +22,7 @@ test('The that the selector for the checkboxes is not an empty NodeList.', () =>
     expect(SearchFilter.filterCBs.length).not.toBe(0);
 });
 
-test('The that the selector for the checkboxes is an empty NodeList.', () => {
+test('Tests that the selector for the checkboxes is an empty NodeList.', () => {
     document.body.innerHTML = `
         <form id="form-site-search">
             <fieldset>
@@ -40,4 +40,62 @@ test('The that the selector for the checkboxes is an empty NodeList.', () => {
     SearchFilter.init();
 
     expect(SearchFilter.filterCBs.length).toBe(0);
+});
+
+test('Tests the restaurant filter results in only 2 results.', () => {
+    document.body.innerHTML = `
+        <form id="form-site-search">
+            <fieldset>
+                <input type="checkbox" value="restaurant" />
+            </fieldset>
+        </form>
+
+        <ul id="site-search-results">
+            <li data-post-type="restaurant"></li>
+            <li data-post-type="page"></li>
+            <li data-post-type="restaurant"></li>
+            <li data-post-type="post"></li>
+        </ul>
+    `;
+
+    SearchFilter.init();
+
+    // Finds the checkbox, checks it, then fires an input event.
+    const restaurantCB = document.querySelector('#form-site-search [type=checkbox]');
+    restaurantCB.checked = true;
+    restaurantCB.dispatchEvent(new Event('input'));
+
+    // Gets the elements that were not filtered out.
+    const nonFilteredElems = document.querySelectorAll('#site-search-results > :not([hidden])');
+
+    expect(nonFilteredElems.length).toBe(2);
+});
+
+test('Tests the page filter results in only 1 result.', () => {
+    document.body.innerHTML = `
+        <form id="form-site-search">
+            <fieldset>
+                <input type="checkbox" value="page" />
+            </fieldset>
+        </form>
+
+        <ul id="site-search-results">
+            <li data-post-type="restaurant"></li>
+            <li data-post-type="page"></li>
+            <li data-post-type="restaurant"></li>
+            <li data-post-type="post"></li>
+        </ul>
+    `;
+
+    SearchFilter.init();
+
+    // Finds the checkbox, checks it, then fires an input event.
+    const restaurantCB = document.querySelector('#form-site-search [type=checkbox]');
+    restaurantCB.checked = true;
+    restaurantCB.dispatchEvent(new Event('input'));
+
+    // Gets the elements that were not filtered out.
+    const nonFilteredElems = document.querySelectorAll('#site-search-results > :not([hidden])');
+
+    expect(nonFilteredElems.length).toBe(1);
 });
