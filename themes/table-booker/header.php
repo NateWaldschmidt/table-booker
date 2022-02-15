cbe57406ad234e2b32914e448df658411a5c6b07
NateWaldschmidt <nate.waldschmidt@bps101.net>
2022-02-14 20:55:30 -0600

    The header now detects if the user is currently logged in.

--- themes/table-booker/header.php
+++ themes/table-booker/header.php
@@ -13,10 +13,22 @@
 		<a id="home-logo-link" href="<?php echo bloginfo('url'); ?>"><?php echo bloginfo('name'); ?></a>
 		<!-- <button type="button">+</button> -->
 	</div>
-	<div class="account-container">
-		<a id="header-login" href="<?php echo esc_url(bloginfo('url').'/login') ?>">Login</a>
-		<a id="header-signup" href="<?php echo esc_url(bloginfo('url').'/sign-up') ?>">Sign Up</a>
-	</div>
+	<?php if ( is_user_logged_in() ): ?>
+		<div class="account-container">
+			<?php
+			global $current_user;
+			wp_get_current_user();
+			?>
+			<p id="header-login">
+				<?php echo esc_html($current_user->display_name); ?>
+			</p>
+		</div>
+	<?php else: ?>
+		<div class="account-container">
+			<a id="header-login" href="<?php echo esc_url(bloginfo('url').'/login') ?>">Login</a>
+			<a id="header-signup" href="<?php echo esc_url(bloginfo('url').'/sign-up') ?>">Sign Up</a>
+		</div>
+	<?php endif; ?>
 </header>
 
 <div id="tb-content">
